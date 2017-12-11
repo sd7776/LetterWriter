56dcc0febfb7bbdd811c50f78ba399455beeaca8
Sourav <sdebnath600@gmail.com>
2017-12-10 20:23:39 -0500

    HeadFirstJava

--- src/DrumKitTestDrive.java
+++ src/DrumKitTestDrive.java
@@ -1,2 +1,11 @@
 public class DrumKitTestDrive {
+    public static void main(String[] args){
+        DrumKit d = new DrumKit();
+        d.playSnare();
+        d.playTopHat();
+        d.snare = false;
+        if(d.snare == true){
+            d.playSnare();
+        }
+    }
 }
